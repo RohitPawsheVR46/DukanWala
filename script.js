583fc55b2332cddbb8af82b289e3eebf9e9b5865
Rohitvr4 <rohitvrpawshe@gmail.com>
2026-08-23 23:02:49 +0530

    create products.html

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 // 2. Category filtering
 // 3. Add to cart
 // 4. Cart quantity + total
+
+let cards =[];
+
+let search = document.querySelector("#searchbar input");
+search.addEventListener("input" , function(){
+    let filter = cards.filter(function(card){
+        return card.namee.startsWith(search.value);
+    })
+})
